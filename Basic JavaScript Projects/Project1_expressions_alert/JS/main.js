de4aa5515b67fb5ad24350d184e4530f42fe5a23
Kari Fuller <kari.christine.fuller@gmail.com>
2022-06-29 16:35:12 -0700

    Created main.js and updated index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,4 @@
+function My_First_Function() { //function and naming the function
+    var A = (5+7) //naming the variable A with parameters 5+7
+    document.write(A) //execute function - write what var A represents
+}
