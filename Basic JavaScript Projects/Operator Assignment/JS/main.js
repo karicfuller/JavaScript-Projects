f4daff368d4e84ae2333cfeec8febf1ef846118e
Kari Fuller <kari.christine.fuller@gmail.com>
2022-06-30 13:31:20 -0700

    Created/tested Operations Assignment

---
+++ Basic JavaScript Projects/Operator Assignment/JS/main.js
@@ -0,0 +1,6 @@
+function conFunction() {
+    var words = "OMG! Look,";
+    words += " It works!";
+    document.getElementById("con").innerHTML = words;
+
+}
--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,4 +1,5 @@
 var i = "I think I might be getting this JavaScript stuff.";
 var b = " Although I am still confused about a lot."
     document.write(i+b);
-    
+
+
