c62f5e4d99080368a875302d7559498b892f12ae
Kari Fuller <kari.christine.fuller@gmail.com>
2022-06-30 11:43:17 -0700

    Completed/tested JavaScript Project Assignment

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,11 @@
-function My_First_Function() { //function and naming the function
-    var A = (5+7) //naming the variable A with parameters 5+7
-    document.write(A) //execute function - write what var A represents
-}
+var sent1 = "This is the beginning of the string"; //this is the beginning of the sentence
+var sent2 = " and this is the end of the string."; //this part of the sentence will be connected to the beginning of the sentence
+    document.write(sent1.concat(sent2));
+
+var java = "This JavaScript stuff is confusing!" //this will show up in the pop-up box
+    window.alert(java)
+
+
+
+
+
