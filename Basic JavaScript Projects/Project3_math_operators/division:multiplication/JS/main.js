685218ab65c69c41aae2ed2d3893161a68abca8b
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-01 12:10:28 -0700

    Created/tested modulus operator

---
+++ Basic JavaScript Projects/Project3_math_operators/division:multiplication/JS/main.js
@@ -0,0 +1,4 @@
+function both() {
+    var multidiv = (3*3) + 6 / 1 - 3;
+    document.getElementById("math").innerHTML = "3 multiplied by 3, plus 6, divided by 1 subtracted by 3 equals " + multidiv; 
+}
---
+++ Basic JavaScript Projects/Project3_math_operators/modulus operator/JS/main.js
@@ -0,0 +1,4 @@
+function modulus() {
+    var oper = 10 % 3;
+    document.getElementById("math").innerHTML = "When you divide 10 by 3 you have a remainder of: " + oper; 
+}
