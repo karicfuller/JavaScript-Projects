ac8a81fd6c302d47c444f4e0e68d6535090f0b4f
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-01 09:41:02 -0700

    Updated multiplication to addition

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,4 +1,4 @@
-function mathfunction() {
-    var multiplication = 3 * 3;
-    document.getElementById("math").innerHTML = "3*3 = " + multiplication; 
+function adding() {
+    var adding = 3 + 3;
+    document.getElementById("math").innerHTML = "3+3 = " + adding; 
 }
