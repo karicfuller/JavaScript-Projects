b04fb5efb01067f06b2680235fbaa6f82f298579
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-02 14:03:35 -0700

    Created/tested dictionary assignment

---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,10 @@
+function a_dictionary() {
+    var My_Car = {
+        Make:"Honda",
+        Model:"CRV",
+        Year:2019,
+        Color:"Dark Green",
+        Feature:"AWD"
+    };
+    document.getElementById("Dictionary").innerHTML = My_Car.Model;
+}
