154ee22316b1908e97036a05621ba9017c99fb68
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-02 15:37:28 -0700

    Updated Dictionary Assignment with Delete

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,22 @@
-function a_dictionary() {
-    var My_Car = {
-        Make:"Honda",
-        Model:"CRV",
-        Year:2019,
-        Color:"Dark Green",
-        Feature:"AWD"
+function a_dictionary() { //named function "a_dictionary"
+    var My_Car = { //named variable "My_Car"
+        Make:"Honda", //KVP
+        Model:"CRV", //KVP
+        Year:2019, //KVP
+        Color:"Dark Green", //KVP
+        Feature:"AWD" //KVP
     };
-    document.getElementById("Dictionary").innerHTML = My_Car.Model;
+    document.getElementById("Dictionary").innerHTML = My_Car.Model; //calling for the Model of My-Car
+}
+
+function the_dictionary() { //named function "the_dictionary"
+    var My_Car = { //named variable My_Car
+        Make:"Honda", //KVP
+        Model:"CRV", // KVP
+        Year:2019, //KVP
+        Color:"Dark Green", //KVP
+        Feature:"AWD" //KVP
+    };
+    delete My_Car.Model; //deleting Model key
+    document.getElementById("Dictionary2").innerHTML = My_Car.Model; //result should be undefined as key has been deleted
 }
