f56ab1b511c423baf5ddb90fc9663e63d511d3c5
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-05 15:43:06 -0700

    Added notes to main.js

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -2,7 +2,7 @@ function vote_age() { //named function "vote_age"
     var Age, Can_vote;
     Age = document.getElementById("the_age").value;
     Can_vote = (Age >= 18) ? "You are of age":"You are too young"; //can vote if age 18 or older
-    document.getElementById("old_enough").innerHTML = Can_vote +  " to vote.";
+    document.getElementById("old_enough").innerHTML = Can_vote +  " to vote."; //result will show if old enough to vote
 }
 
 ///////////////////
@@ -10,8 +10,8 @@ function vote_age() { //named function "vote_age"
 function Friend(Hair, Eyes, Age, Race) { //named function "Friend"
     this.Friend_Hair = Hair; //hair color
     this.Friend_Eyes = Eyes; //eye color
-    this.Friend_Age = Age;
-    this.Friend_Race = Race;
+    this.Friend_Age = Age; //age
+    this.Friend_Race = Race; //race
 }
 
 var Oscar = new Friend("black", "brown", 46, "Native American"); //black hair, brown eyes, age 46, Native American
@@ -20,17 +20,17 @@ var Jay = new Friend("brown", "green", 57, "Hispanic");
 function myFunction() { //named function "myFunction"
     document.getElementById("New_and_This").innerHTML = 
     "Oscar's hair color is " + Oscar.Friend_Hair + ", his eyes are " + Oscar.Friend_Eyes + 
-    ", he is " + Oscar.Friend_Age + " years old, and he is " + Oscar.Friend_Race;
+    ", he is " + Oscar.Friend_Age + " years old, and he is " + Oscar.Friend_Race; //result is all 4 characteristics
 }
 
 ////////////////////
 
-function age_Function() {
+function age_Function() { //named function "age_Function"
     document.getElementById("Age").innerHTML = theAge();
-    function theAge() {
+    function theAge() { //nested function name "theAge"
         var yourAge = 10;
-        function addTwo() {yourAge +=2;}
+        function addTwo() {yourAge +=2;} //add 2 to 10
         addTwo();
-        return yourAge;
+        return yourAge; //result should be 12
     }
 }
