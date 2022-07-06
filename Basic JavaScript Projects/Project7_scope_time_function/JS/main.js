a217caa48daa5207ce715da9e6bf9daaab900aaa
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-05 18:53:01 -0700

    Incomplete Project7 - no local variable error

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -2,28 +2,43 @@
 
 var x = 10; //named variable "x" with value 10
 function Numbers_added_1() { //named function
-    document.write(35 + x + "<br>"); //result should be 45
+    document.write(35 + x + "<br>"); //result 45
 }
 function Numbers_added_2() { //named function
-    document.write(x+100); //result should bee 110
+    document.write(x + 100 + "<br>"); //result 110
 }
 Numbers_added_1();
 Numbers_added_2();
+
 
 //Local Variable
 
 function Numbers_to_add1() { 
     var x = 25;
-    document.write(30 + x + "<br>");
+    document.write(x + 30 + "<br>"); //result 55
 }
 function Numbers_to_add2() {
     var y = 30;
-    document.write(y + 20);
+    document.write(y + 20 + "<br>"); //result 50
 }
 Numbers_to_add1();
 Numbers_to_add2();
 
-//////////////////////////////////////////
+
+//Local Variable w/error
+
+function Added_Numbers1() {
+    var x = 30;
+    document.write(x + 10 + "<br>") //result 40
+}
+function Added_Numbers2() {
+    console.log(x + 10); //nothing, error no variable value
+}
+Added_Numbers1();
+Added_Numbers2();
+
+
+//Conditional Statements
 
 function the_Date() {
     if (new Date().getHours() < 20) {
@@ -34,13 +49,31 @@ function the_Date() {
 
 //if/else Statements
 
-function The_Age() {
+function The_Age() { //named function "The_Age"
     Age = document.getElementById("Years_Old").value;
-    if (Age >= 21) {
+    if (Age >= 21) { //if 21 or older
         Drink = "What do you want to drink?";
     }
-    else {
+    else { //else too young
         Drink = "You are not old enough to drink. Nice try!"
     }
     document.getElementById("Your_Age").innerHTML = Drink;
+}
+
+
+//else/if Statements
+
+function Time_function () { 
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time < 18) {
+        Reply = "It is afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }
