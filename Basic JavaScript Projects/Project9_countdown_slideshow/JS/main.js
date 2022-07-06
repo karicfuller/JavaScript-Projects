0f0ee68d45f47d552a0fd4855854aee5ec307b12
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-06 16:57:29 -0700

    Created/tested slideshow

--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -18,3 +18,17 @@ function countdown() {
 
 //slideshow
 
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 2000); // change image every 2 seconds
+}
