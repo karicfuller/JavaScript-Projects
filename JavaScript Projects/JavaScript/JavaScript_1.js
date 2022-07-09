039559fafe48f61645d874d9e5dcf5d352c3894a
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-09 06:57:45 -0700

    Added <canvas> element and gradient method

--- JavaScript Projects/JavaScript/JavaScript_1.js
+++ JavaScript Projects/JavaScript/JavaScript_1.js
@@ -1,3 +1,5 @@
+//switch method
+
 function food_Function() {
     var pick_food;
     var food = document.getElementById("food_Choice").value;
@@ -20,3 +22,30 @@ function food_Function() {
     }
     document.getElementById("Output").innerHTML = pick_food;
 }
+
+//getElementByClassName()
+
+function Hello_World_Function() {
+    var A = document.getElementsByClassName("Click");
+    A[0].innerHTML = "The text has changed!";
+}
+
+//<canvas> Element
+
+var c = document.getElementById("my_Canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(0,0);
+ctx.lineTo(200, 100);
+ctx.stroke();
+
+//canvas createLinearGradient() method
+
+var c = document.getElementById("theCanvas");
+var ctx = c.getContext("2d");
+
+var grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
