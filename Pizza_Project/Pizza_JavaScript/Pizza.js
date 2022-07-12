e397dac30558a418b1337bc9a015161ac1ac3592
Kari Fuller <kari.christine.fuller@gmail.com>
2022-07-12 14:54:22 -0700

    Completed/tested Pizza project

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -34,9 +34,9 @@ function getTopping(runningTotal,text1) {
     var toppingTotal = 0;
     var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
-    for (var j = 0; j < toppingArray.length; j++);{
+    for (var j = 0; j < toppingArray.length; j++) {
         if (toppingArray[j].checked) {
-            selectedTopping.push("toppingArray[j].value");
+            selectedTopping.push(toppingArray[j].value);
             console.log("selected topping item: ("+toppingArray[j].value+")");
             text1 = text1+toppingArray[j].value+"<br>"
         }
@@ -44,15 +44,14 @@ function getTopping(runningTotal,text1) {
     var toppingCount = selectedTopping.length;
     if (toppingCount > 1) {
         toppingTotal = (toppingCount - 1);
-        else {
+    } else {
             toppingTotal = 0;
         }
     runningTotal = (runningTotal + toppingTotal);
-    console.log("total selected topping items: "+toppingCount+".00");
+    console.log("total selected topping items: "+toppingCount);
     console.log(toppingCount+" topping - 1 free topping = "+"$"+toppingTotal+".00");
     console.log("topping text1: "+text1);
     console.log("Purchase Total: "+"$"+runningTotal+".00");
     document.getElementById("showText").innerHTML = text1;
-    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>"
-
-}
+    document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+};
